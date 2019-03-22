c6674a1e242fd3f0c11b655830857b85466a4771
Aladji <aladji.faye@telecom-paristech.fr>
2019-03-22 12:01:42 +0100

    d

--- GPU/videofilter/videofilter.cpp
+++ GPU/videofilter/videofilter.cpp
@@ -159,28 +159,36 @@ Mat executeConvolution(Mat& inputMat, float * filterArray) {
 	Mat outputMat;
 
   inputMat.convertTo(inputMat, CV_32FC1);
+	printf("check");
 
 	input = (float *)clEnqueueMapBuffer(queue, input_buf, CL_TRUE,
 	CL_MAP_WRITE,0,640*360* sizeof(float),0,NULL,&write_event[0],&errcode);
 	checkError(errcode, "Failed to map input");
+	printf("check1");
 
 	filter = (float *)clEnqueueMapBuffer(queue, filter_buf, CL_TRUE,
 	CL_MAP_WRITE,0,3*3* sizeof(float),0,NULL,&write_event[1],&errcode);
 	checkError(errcode, "Failed to map filter");
+	printf("check2");
 
 	output = (float *)clEnqueueMapBuffer(queue, output_buf, CL_TRUE,
 			CL_MAP_READ, 0,640*360* sizeof(float),  0, NULL, NULL,&errcode);
 	checkError(errcode, "Failed to map output");
+	printf("check3");
 
 
 
   //input = (float*)inputMat.data;
 	memcpy(input, (float*)inputMat.data,640*360*sizeof(float));
+	printf("check4");
+
   memcpy(filter,filterArray,3*3*sizeof(float));
+	printf("check5");
 
   clEnqueueUnmapMemObject(queue, input_buf, input, 0, NULL, NULL);
   clEnqueueUnmapMemObject(queue, filter_buf, filter, 0, NULL, NULL);
   clEnqueueUnmapMemObject(queue, output_buf, output, 0, NULL, NULL);
+	printf("check6");
 
   const size_t global_work_size[2] = {640,360};
   status = clEnqueueNDRangeKernel(queue, kernel, 2, NULL,
